1948a2d9ff820667bcf77f4b83b9de94da468a50
Stan Mayer <sthumor@gmail.com>
2024-10-06 19:03:43 +0300

    HW_33_IO_Streams

--- src/main/java/telran/persistence/CodeCommentsSeparation.java
+++ src/main/java/telran/persistence/CodeCommentsSeparation.java
@@ -1,15 +1,50 @@
 package telran.persistence;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CodeCommentsSeparation {
-public static void main(String[] args) {
-    //TODO - data from args[0] split to two files: args[1], args[2]
-    //for sake of simplicity comments may be only on one line, like comments at this file
-    // /* */ cannot be
-    // code ...// comment .... cannot be
-         //However // may be not only at beginning of line, like this
-    //args[0] - path to file containing code and comments 
-    //args[1] - path to file for placing only code
-    //args[2] - path to file for placing only comments
-    
-}
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException(
+                "Fill all the obligatory fields: inputFile outputsComments outputCode");
+        } else {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            Writer output_comments = new BufferedWriter(new FileWriter(args[1]));
+            Writer output_code = new BufferedWriter(new FileWriter(args[2]));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if(line.contains("//")) {
+                    commentCodeSplitter(line, output_comments, output_code);
+                }
+                else {
+                    output_code.append(line + "\n");
+                }
+                
+            }
+            reader.close();
+            output_comments.close();
+            output_code.close();
+        }
+
+    }
+
+    private static void commentCodeSplitter(String line, Writer output_comments, Writer output_code) throws IOException {
+        String pattern = "^ *(?<comment>\\/\\/.*$)";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(line);
+        if(m.find()) {
+            output_comments.append(m.group("comment") + "\n");
+        }
+        else {
+            output_code.append(line + "\n");
+        }
+    }
+
 }
